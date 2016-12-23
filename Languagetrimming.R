9f4783417e0990c2e81e25993249cab854cf58df
sskingdon <b00505028@ntu.edu.tw>
2016-12-23 18:27:10 +0800

    2016/12/23 18:26 Aaaaaaaaarg

---
+++ Languagetrimming.R
@@ -0,0 +1,41 @@
+library(tm)
+path <- "~/tryy/The Philosopher's Stone.txt"
+text <- readLines(path,encoding="UTF-8")
+head(text,40)
+page <-"(Page | [0-9]* Harry Potter and the Philosophers Stone - J.K. Rowling)"
+text <-str_replace_all(text,page,"")
+# text
+vs <- VectorSource(text)
+# NOW The text variable is an array of the lines of the statement.
+txt <-Corpus(vs)
+txt <-tm_map(txt, tolower)
+txt <-tm_map(txt, removePunctuation)
+txt <-tm_map(txt, removeNumbers)
+
+
+# inspect(txt) inspect(dtm)
+
+txt <- tm_map(txt, removeWords, stopwords("english"))
+txt <- tm_map(txt, PlainTextDocument)
+# str(txt)
+ dtm <- TermDocumentMatrix(txt)
+ inspect(dtm[1:13,1:10])
+ str(dtm)
+ 
+ 
+ 
+ 
+ 
+ 
+ (freq.terms <- findFreqTerms(dtm,150))
+
+term.freq <- rowSums(as.matrix(dtm))
+term.freq <- subset(term.freq, term.freq >= 150)
+df <- data.frame(term = freq.terms, freq = term.freq)
+ 
+ 
+ # library(Rgraphviz)
+ # plot(dtm, terms = findFreqTerms(dtm, lowfreq = 150)[1:10], corThreshold = 0.5)
+ library(ggplot2)
+ ggplot(df,aes(x = term, y = freq)) + geom_bar(stat = "identity")
+ 
